da5072bc1158e023163a8c66210edd8fec9c2a4c
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-04 18:55:41 +0100

    Add implementation to list n primes.

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+
+    while number_of_primes:
+        if isPrime(number):
+            list.append(number)
+            number_of_primes = number_of_primes - 1
+            number = number + 1
+        else:
+            number = number + 1
     return list
+
+def isPrime(number):
+    if number == 2:
+        return True
+    else:
+        for value in range(2, number):
+            if (number % value) == 0:
+                return False
+            else:
+                return True
+    return False
